7af5436e2bcb33ae083cd982f741ec870ffc40f2
iyuli <38270397+tuyangJs@users.noreply.github.com>
2025-02-14 10:05:27 +0000

    修改封面

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { ConfigProvider, Flex, Layout, Segmented, Tag, theme } from 'antd';
-import { FC, PropsWithChildren, ReactNode, use, useState } from 'react';
-import wukong from './assets/game.webp';
-import minecraft from './assets/game.jpg';
-import './App.css';
-import { AppstoreOutlined, BarsOutlined } from '@ant-design/icons';
+import { ConfigProvider, Flex, Layout, Segmented, Tag, theme } from "antd";
+import { FC, PropsWithChildren, ReactNode, use, useState } from "react";
+import wukong from "./assets/game.webp";
+import "./App.css";
+import { AppstoreOutlined, BarsOutlined } from "@ant-design/icons";
 
 const App = () => {
-  const [TagLayout, setTagLayout] = useState<MyTagProps['TagLayout']>('Horizontal');
+  const [TagLayout, setTagLayout] =
+    useState<MyTagProps["TagLayout"]>("Horizontal");
   return (
     <ConfigProvider
       theme={{
@@ -20,83 +20,90 @@ const App = () => {
           value={TagLayout}
           onChange={setTagLayout}
           options={[
-            { value: 'List', icon: <AppstoreOutlined /> },
-            { value: 'Horizontal', icon: <BarsOutlined /> },
+            { value: "List", icon: <AppstoreOutlined /> },
+            { value: "Horizontal", icon: <BarsOutlined /> },
           ]}
         />
         <div className="cover">
           <img alt="game" src={wukong} />
           <Tags
             style={{
-              position: 'absolute',
+              position: "absolute",
               bottom: 15,
               right: 15,
             }}
             TagLayout={TagLayout}
             bordered={false}
             color="orange"
-            subtitle="送豪礼">
+            subtitle="送豪礼"
+          >
             预购
           </Tags>
         </div>
         <div className="cover">
-          <img alt="game" src={minecraft} />
+          <img
+            alt="game"
+            src="https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Homepage_Discover-our-games_MC-Vanilla-KeyArt_864x864.jpg"
+          />
           <Tags
             style={{
-              position: 'absolute',
+              position: "absolute",
               bottom: 10,
               right: 10,
             }}
             TagLayout={TagLayout}
             bordered={false}
             color="green"
-            subtitle="¥288">
+            subtitle="¥288"
+          >
             预购
           </Tags>
         </div>
-        <Tags
-          bordered={false}
-          color="green">
+        <Tags bordered={false} color="green">
           预购
         </Tags>
       </Layout>
     </ConfigProvider>
   );
-}
+};
 
 // 继承 antd 的 TagProps，并支持 children
-interface MyTagProps extends PropsWithChildren, React.ComponentProps<typeof Tag> {
-  subtitle?: ReactNode
-  TagLayout?: 'List' | 'Horizontal'
+interface MyTagProps
+  extends PropsWithChildren,
+    React.ComponentProps<typeof Tag> {
+  subtitle?: ReactNode;
+  TagLayout?: "List" | "Horizontal";
 }
 
 const Tags: FC<MyTagProps> = (props) => (
   <Flex
     className="tags"
     style={props.style}
-    vertical={props.TagLayout === 'List'}
+    vertical={props.TagLayout === "List"}
   >
     <Tag
-      className='tagt'
+      className="tagt"
       {...props}
       style={{
         marginInlineEnd: 0,
-        alignItems: 'center',
-        textAlign: 'center',
-        padding:4
+        alignItems: "center",
+        textAlign: "center",
+        padding: 4,
       }} //不再继承style
-    >{props.children}</Tag>
-    {props.subtitle
-      &&
+    >
+      {props.children}
+    </Tag>
+    {props.subtitle && (
       <span
         style={{
-          color: 'white',
-          padding:4,
-        }}>
+          color: "white",
+          padding: 4,
+        }}
+      >
         {props.subtitle}
       </span>
-    }
-  </Flex >
+    )}
+  </Flex>
 );
 
 export default App;
